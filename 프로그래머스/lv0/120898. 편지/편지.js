d8b1adb4cec03df57799aaab4dbbef978dec4ccc
김철우 <102791231+chulwoo8747@users.noreply.github.com>
2023-09-21 17:14:07 +0900

    [level 0] Title: 편지, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120898. 편지/편지.js
+++ 프로그래머스/lv0/120898. 편지/편지.js
@@ -1,7 +1,7 @@
 function solution(message) {
-    let a = 0;
+    let length = 0;
     for (let i = 0; i < message.length; i++) {
-        a = message.length*2
+        length = message.length*2
     }
-    return a;
+    return length;
 }
